2867a22c1c53d7446126a63d44b3f3bdd16b0a10
aerw93 <Aerw93@gmail.com>
2021-02-01 02:58:24 +0200

    Heder + footer

--- Ktn_Portfolio/js/script.js
+++ Ktn_Portfolio/js/script.js
@@ -21,6 +21,53 @@ ibg();
 //  Шапка скролл
  window.addEventListener('scroll', function() {
    let header = document.querySelector('.header');
-   header.classList.toggle('sticky', window.scrollY > 0 );
+   header.classList.toggle('sticky', window.scrollY > 0);
+   logoChange();
 });
-// Padding for Scroll-y
+// Contacts toogleMenu 
+(function () {
+   var click = document.getElementById('toggle-contacts');
+   click.addEventListener('click', function (event) {
+     event.preventDefault();
+     var menu = document.getElementById('contacts');
+     menu.classList.toggle('open');
+   });
+ })();
+ // Function logo-img src=change
+ function logoChange () {
+   const logoImg = document.getElementById('logo-change');
+   if (window.scrollY > 0) {
+      logoImg.src = "img/Logo/Logo_KTN_4_2.png";
+   } else {
+      logoImg.src = "img/Logo/Logo_KTN_4_3.png";
+   }
+ };
+// Function hide- showeds Contacts_items on click over
+const btnContacts = document.querySelector('.icon-email');
+const menuContacts = document.querySelector('.follow__content');
+
+ document.addEventListener('click', function(e) {
+    const target = e.target;
+    const its_menu = target == menuContacts || menuContacts.contains(target);
+    const its_btnMenu = target == btnContacts;
+    const menu_is_active = menuContacts.classList.contains('open');
+
+    if (!its_menu && !its_btnMenu && menu_is_active) {
+      menuContacts.classList.remove('open');
+  }
+ });
+
+// Function hide-nav_menu on click over menu
+const btnBurger = document.querySelector('.toggle-menu');
+const menuBurger = document.querySelector('.menu__body');
+
+ document.addEventListener('click', function(e) {
+    const target = e.target;
+    const its_menu = target == menuBurger || menuBurger.contains(target);
+    const its_btnMenu = target == btnBurger;
+    const menu_is_active = menuBurger.classList.contains('active');
+
+    if (!its_menu && !its_btnMenu && menu_is_active) {
+      menuBurger.classList.remove('active');
+  }
+ });
