231c3b4629160f9507156c41151edff222bba536
aerw93 <Aerw93@gmail.com>
2021-02-18 15:46:31 +0200

    src

--- Ktn_Portfolio/js/script.js
+++ Ktn_Portfolio/js/script.js
@@ -1,14 +1,4 @@
-function ibg(){
 
-let ibg=document.querySelectorAll(".ibg");
-for (var i = 0; i < ibg.length; i++) {
-if(ibg[i].querySelector('img')){
-ibg[i].style.backgroundImage = 'url('+ibg[i].querySelector('img').getAttribute('src')+')';
-}
-}
-}
-
-ibg();
 // Меню бургер
 (function () {
    var click = document.getElementById('menu-active');
@@ -46,15 +36,6 @@ ibg();
      menu.classList.toggle('open');
    });
  })();
- // Function logo-img src=change
- function logoChange () {
-   const logoImg = document.getElementById('logo-change');
-   if (window.scrollY > 0) {
-      logoImg.src = "../img/Logo/Logo_KTN_4_2.png";
-   } else {
-      logoImg.src = "../img/Logo/Logo_KTN_4_3.png";
-   }
- };
 // Function hide- showeds Contacts_items on click over
 const btnContacts = document.querySelector('.icon-email');
 const menuContacts = document.querySelector('.follow__content');
@@ -69,26 +50,16 @@ const menuContacts = document.querySelector('.follow__content');
       menuContacts.classList.remove('open');
   }
  });
-
-// Function hide-nav_menu on click over menu
-//const btnBurger = document.querySelector('.toggle-menu');
-//const menuBurger = document.querySelector('.menu__body');
-
-// document.addEventListener('click', function(e) {
-//    const target = e.target;
-//    const its_menu = target == menuBurger || menuBurger.contains(target);
-//    const its_btnMenu = target == btnBurger;
-//    const menu_is_active = menuBurger.classList.contains('active');
-
-//    if (!its_menu && !its_btnMenu && menu_is_active) {
-//      menuBurger.classList.remove('active');
-//  }
-// });
-
 //
 // Анимация при скроле 
 //==================================================================================================
 //==================================================================================================
+ // Function logo-img src=change
+ //==================================================================================================
+ //==================================================================================================
+  // Function ibg-img
+ //==================================================================================================
+ //==================================================================================================
 
 const animItems = document.querySelectorAll('.anim-items');
 
@@ -126,3 +97,21 @@ if (animItems.length > 0) {
       animOnScroll();
    }, 300);
 }
+function logoChange () {
+   const logoImg = document.getElementById('logo-change');
+   if (window.scrollY > 0) {
+      logoImg.src = "./img/Logo/Logo_KTN_4_2.png";
+   } else {
+      logoImg.src = "./img/Logo/Logo_KTN_4_3.png";
+   }
+ };
+function ibg(){
+
+   let ibg=document.querySelectorAll(".ibg");
+   for (var i = 0; i < ibg.length; i++) {
+   if(ibg[i].querySelector('img')){
+   ibg[i].style.backgroundImage = 'url('+ibg[i].querySelector('img').getAttribute('src')+')';
+   }
+   }
+   }
+   ibg();
